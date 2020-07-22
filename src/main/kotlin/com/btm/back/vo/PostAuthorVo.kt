f2c30cbc8f3b0ddb90478f82d9bdbdea2404fb20
wangbo <wangbo@id.link>
2020-07-22 20:51:49 +0800

    修改返回类字段

--- src/main/kotlin/com/btm/back/vo/PostAuthorVo.kt
+++ src/main/kotlin/com/btm/back/vo/PostAuthorVo.kt
@@ -8,6 +8,7 @@ class PostAuthorVo {
     var icon: String? = null
     var id: Int? = null
     var address: String? = null
+
     override fun toString(): String {
         return "PostAuthorVo(nickName=$nickName, icon=$icon, id=$id, address=$address)"
     }
